1acbe9adf7686ee3f2fe71567c5cce7e785a739c
Denilson <61381742+denilsonssilva@users.noreply.github.com>
2024-08-27 21:06:14 -0300

    desconto-mercado

--- Hello_World.py
+++ Hello_World.py
@@ -1,1 +1,10 @@
-print('Hello World!!!!')
+valor = float(input('Preço do produto: '))
+qtd = int(input('Quantidade: '))
+total = valor * qtd
+
+if total > 200 or qtd >= 25:
+    desconto = total * 0.08
+    total -= desconto
+    print(f'O valor final com desconto é {total:.2f}')
+else:
+    print(f'O valor final é: {total:.2f}')
